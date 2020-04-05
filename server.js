e342f792ff70158b24e78fdd989af3d67dd25427
Syro4444 <56363527+Syro4444@users.noreply.github.com>
2020-04-05 21:42:34 +0200

    facto des Fonctions d'erreurs

--- server.js
+++ server.js
@@ -11,52 +11,62 @@ let error;
 const app = express();
 app.use(express.json());
 
+async function usernameExistsSignup(username) {
+    const usersCreated = await collectionUsers.find({username: {$eq: username}}).toArray();
+    if (usersCreated.length !== 0) {
+        error = "Cet identifiant est deja associe a un compte";
+        return error;
+    }
+}
+
+async function usernameExistsSignin(username) {
+    const usersLogin = await collectionUsers.find({username: {$eq: username}}).toArray();
+    if (usersLogin.length == 0) {
+        error = "Cet identifiant est inconnu";
+        return error;
+    }
+}
+
+function passwordLength(password) {
+    if (password.length < 4) {
+        error = "Le mot de passe doit contenir au moins 4 caracteres";
+        return error;
+    }
+}
+
+function usernameLength(username) {
+    if (username.length < 2 || username.length > 20) {
+        error = "Votre identifiant doit contenir entre 2 et 20 caractères";
+        return error;
+    }
+}
+
+function usernameIsMin(username) {
+    let position = 0;
+    while (position < username.length) {
+        if (username.charCodeAt(position) < 97 || username.charCodeAt(position) > 122) {
+            error = "Votre identifiant ne doit contenir que des lettres minuscules non accentuées";
+            position = username.length;
+        } else {
+            position++;
+        }
+    }
+}
+
 app.post('/signup', async function (req, res) {
-
-    async function usernameExists(username) {
-        const usersCreated = await collectionUsers.find({username: {$eq: username}}).toArray();
-        if (usersCreated.length !== 0) {
-            error = "Cet identifiant est deja associe a un compte";
-            return error;
-        }
-    }
-
-    function passwordLength(password) {
-        if (password.length < 4) {
-            error = "Le mot de passe doit contenir au moins 4 caracteres";
-            return error;
-        }
-    }
-
-    function usernameLength(username) {
-        if (username.length < 2 || username.length > 20) {
-            error = "Votre identifiant doit contenir entre 2 et 20 caractères";
-            return error;
-        }
-    }
-
-    function usernameIsMin(username) {
-        let position = 0;
-        while (position < username.length) {
-            if (username.charCodeAt(position) < 97 || username.charCodeAt(position) > 122) {
-                error = "Votre identifiant ne doit contenir que des lettres minuscules non accentuées";
-                position = username.length;
-            } else {
-                position++;
-            }
-        }
-    }
 
     const username = req.body.username;
     const password = req.body.password;
 
+    error = null;
+
     await passwordLength(password);
     await usernameLength(username);
     await usernameIsMin(username);
-    await usernameExists(username);
+    await usernameExistsSignup(username);
 
 
-    if (error === undefined) {
+    if (error === null) {
         const user = {username: username, password: password};
 
         await collectionUsers.insertOne(user);
@@ -70,56 +80,21 @@ app.post('/signup', async function (req, res) {
 
 app.post('/signin', async function (req, res) {
 
-    async function usernameExists(username) {
-        const usersLogin = await collectionUsers.find({username: {$eq: username}}).toArray();
-        if (usersLogin.length == 0) {    
-            error = "Cet identifiant est inconnu";
-            return error;
-        }
-    }
-
-    function passwordLength(password) {
-        if (password.length < 4) {
-            error = "Le mot de passe doit contenir au moins 4 caracteres";
-            return error;
-        }
-    }
-
-    function usernameLength(username) {
-        if (username.length < 2 || username.length > 20) {
-            error = "Votre identifiant doit contenir entre 2 et 20 caractères";
-            return error;
-        }
-    }
-
-    function usernameIsMin(username) {
-        let position = 0;
-        while (position < username.length) {
-            if (username.charCodeAt(position) < 97 || username.charCodeAt(position) > 122) {
-                error = "Votre identifiant ne doit contenir que des lettres minuscules non accentuées";
-                position = username.length;
-            } else {
-                position++;
-            }
-        }
-    }
-
     const username = req.body.username;
     const password = req.body.password;
 
     await passwordLength(password);
     await usernameLength(username);
     await usernameIsMin(username);
-    await usernameExists(username);
+    await usernameExistsSignin(username);
 
-
-    if (error === undefined) {
+    if (error === null) {
         const user = {username: username, password: password};
 
         await collectionUsers.insertOne(user);
         res.send("token");
-    } 
-    else if ( erreur == "Cet identifiant est inconnu") {
+    }
+    else if ( error === "Cet identifiant est inconnu") {
         res.status(403).send(error);
     }
 
@@ -129,10 +104,6 @@ app.post('/signin', async function (req, res) {
 
 
 })
-
-
-
-
 
 ;(async () => {
     console.log(`Connecting to ${DATABASE_NAME}...`);
